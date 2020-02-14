53ded8ebd2ea90b5a09c16e31d5dab53178ac9af
alexbuchan <alex.buchan@telefonica.net>
2020-02-14 16:12:04 +0100

    Add passport local authentication strategy

--- app.js
+++ app.js
@@ -3,15 +3,27 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
 let cors = require('cors');
+let passport = require('passport');
+let session = require('express-session');
 
 let users = require('./routes/users');
+
+require('./passport_setup')(passport);
 
 let app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser())
+app.use(cookieParser());
+
+app.use(session({
+  secret: 'secret',
+  resave: false ,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', users);
 
